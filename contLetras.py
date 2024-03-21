76afe4b9190fa9001c9bfb963bf3bc3b4807b739
Artchzo <136608003+Archh0@users.noreply.github.com>
2024-03-21 17:40:05 -0300

    Não há mais duas linhas para o contador de tentativas, apenas uma `tentativa += 1`

--- contLetras.py
+++ contLetras.py
@@ -3,7 +3,6 @@ from random import randint
 randMeta = randint(0,46)
 portMeta = 46
 
-tentativa = 0
 
 palavra = input("Programa iniciado, insira a sua palavra e irei contar os caracteres presentes: ")
 
@@ -11,7 +10,7 @@ while palavra.isalpha() == False:
     palavra = input("Erro, não insira espaços na palavra.\nTente novamente: ")
 
 while len(palavra) < randMeta or len(palavra) < portMeta:
-    tentativa = tentativa + 1
+    tentativa += 1
     palavra = input(f"{tentativa}ª tentativa.\nSua palavra teve {len(palavra)} letras, para sair do loop você deve atingir {randMeta} letras ou {portMeta}.\nInsira a sua palavra: ")
 
 if len(palavra) > randMeta or len(palavra) > portMeta:
